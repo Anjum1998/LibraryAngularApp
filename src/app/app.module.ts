7b881c3408041bc8072fb673b8205bf8a79dc8e0
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-11-24 21:14:15 +0530

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdministratorLoginComponent } from './administrator-login/administrator-login.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdministratorLoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
